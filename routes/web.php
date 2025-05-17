91065e601479610c5f8a6d25cb7aa76b6383bf13
Manas125 <manassengoune@gmail.com>
2025-05-17 14:34:38 +0100

    Manas

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
+Route::resource('communes', 'CommunesController::class');
+    
+
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
